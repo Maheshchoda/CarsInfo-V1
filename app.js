a78a075465eb68ff2cb84e61508d746ee0765f71
Light <Light@Maheshs-MacBook-Pro.local>
2018-06-12 15:59:42 +0530

    small mistake error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express        = require('express'),-
+const express        = require('express'),
       app            = express(),
       bodyParser     = require('body-parser'),
       mongoose       = require('mongoose'),
